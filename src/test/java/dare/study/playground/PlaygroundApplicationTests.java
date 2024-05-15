2d5645ce107aaedd5e9706c1252ea1674be5c987
G-D4R3 <gdare1999@gmail.com>
2024-05-15 17:23:39 +0900

    first commit

---
+++ src/test/java/dare/study/playground/PlaygroundApplicationTests.java
@@ -0,0 +1,13 @@
+package dare.study.playground;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlaygroundApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
